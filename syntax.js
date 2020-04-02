2d5741f7b611b38a91ec1c9bfb1d75655a857b34
Kara Cavanaugh <karacav@msn.com>
2020-04-02 18:27:28 -0400

    Number 1

---
+++ syntax.js
@@ -0,0 +1,5 @@
+// 1. Programming Quiz: Writing a For...of Loop (1-4)
+const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+for (let day of days) {
+    console.log(day.charAt(0).toUpperCase() + day.slice(1));
+} 
