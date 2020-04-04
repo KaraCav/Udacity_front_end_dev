9cfab2dfd7a8b2f8be48c456f08ca425ce54acab
Kara Cavanaugh <karacav@msn.com>
2020-04-04 18:52:37 -0400

    Through number 3

--- syntax.js
+++ syntax.js
@@ -3,3 +3,26 @@ const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 
 for (let day of days) {
     console.log(day.charAt(0).toUpperCase() + day.slice(1));
 } 
+
+// 2. Instructions: Use the spread operator to combine the `fruits` and `vegetables` arrays into the `produce` array.
+const fruits = ["apples", "bananas", "pears"];
+const vegetables = ["corn", "potatoes", "carrots"];
+
+const produce = [...fruits, ...vegetables];
+console.log(produce);
+
+// 3. Use the rest parameter to create an average() function that calculates the average of an unlimited amount of numbers.
+function average(...nums) {
+    let total = 0;
+    for(const num of nums) {
+        total += num;
+    }
+    if(total === 0) {
+        return 0;
+    }
+    else {
+        return total/ nums.length;
+    }
+}
+console.log(average(2, 6));
+console.log(average(2, 3, 3, 5, 7, 10));
