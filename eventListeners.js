f1e892f7c0e3662af949745c96116b86e4dd0bfd
Kara Cavanaugh <karacav@msn.com>
2020-04-08 14:56:06 -0400

    Notes pt 1

---
+++ eventListeners.js
@@ -0,0 +1,8 @@
+// Can add event listeners to the document or elements
+document.addEventListener('click', function () {
+   console.log('The document was clicked');
+});
+
+document.body.addEventListener('click', function () {
+    console.log('The document body was clicked');
+});
